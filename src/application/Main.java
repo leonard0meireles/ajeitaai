f96d706243ef08fabe7c840ca9c6c2879bd8a2e1
Leonardo-Chappie <leomeireles66@gmail.com>
2025-10-26 22:11:19 -0300

    Project created

---
+++ src/application/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Someone, me, mess up and lost all the project and is going to make the first sad commit :(");
+    }
+}
