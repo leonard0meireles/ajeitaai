49a360f69ff40f880f34a1edc09995ea6a2faed5
Leonardo-Chappie <leomeireles66@gmail.com>
2025-10-27 20:38:49 -0300

    fix in -f arg

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,8 @@
 import java.util.List;
+
 /**
  * Main Class
+ * 
  * @author Leonardo
  * @version 0.0.1
  * @since 0.0.1
@@ -12,21 +14,25 @@ public class Main {
             return;
         }
         if (args[0].equals("-h") || args[0].equals("--help")) {
-           Util.printHelp();
+            Util.printHelp();
         }
-        if ((args[0].equals("-f") || args[0].equals("--file")) && (args[2].equals("-o") || args[2].equals("--output"))) {
-            List<String> lines = Util.readFile(args[1]);
-            Util.makeFile(lines, args[3]);
-            return;    
-        }
-        if (args[0].equals("-f") || args[0].equals("--file")) {
-            List<String> lines = Util.readFile(args[1]);
-            Util.makeFile(lines, "output.txt");
-            return;    
+        if (args.length == 2) {
+            if (args[0].equals("-f") || args[0].equals("--file")) {
+                List<String> lines = Util.readFile(args[1]);
+                Util.makeFile(lines, "output.txt");
+                return;
+            }
+        }else if (args.length == 4) {
+            if ((args[0].equals("-f") || args[0].equals("--file"))
+                    && (args[2].equals("-o") || args[2].equals("--output"))) {
+                List<String> lines = Util.readFile(args[1]);
+                Util.makeFile(lines, args[3]);
+                return;
+            }
         }
         if (args[0].equals("-v") || args[0].equals("--version")) {
             System.out.println("ajeitaai version 0.0.1");
         }
-        
+
     }
 }
